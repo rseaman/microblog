9c62ca65655ba93f06ef9c1d715e851f3fb1b076
Ubuntu <ubuntu@ip-10-165-32-113.ec2.internal>
2013-11-14 06:57:38 +0000

    db add, part 4

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 ##Microblog test
 
 Basic test using Flask and some extensions. Learning experiment for mvc frameworks.
-Check out the guide I'm following at http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
--- config.py
+++ config.py
@@ -1,3 +1,18 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 CSRF_ENABLED = True
-skfile = open('../flaskkey')
-SECRET_KEY = skfile.read()
+with open('../flaskkey') as f:
+    key = f.readline()
+SECRET_KEY = str(key)
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https:///www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
