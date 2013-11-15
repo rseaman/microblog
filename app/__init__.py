2770e4d69b766489044c2a0c80e23446a526f535
rseaman <riley.seaman@gmail.com>
2013-11-15 05:07:20 +0000

    beginning of login work

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
+import is
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 app = Flask(__name__)
 app.config.from_object('config')
